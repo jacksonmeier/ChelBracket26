d5625dfcc77d90fb2ef7373bec416b6ba013f5a0
jacksonmeier <jackson.meier@ucalgary.ca>
2026-04-08 13:46:37 -0600

    Fix: set Github owner in config

--- config.js
+++ config.js
@@ -17,7 +17,7 @@
 // =============================================================
 
 window.CHELB_CONFIG = {
-  owner:  'YOUR_GITHUB_USERNAME',   // e.g. 'gretzky99'
+  owner:  'jacksonmeier',
   repo:   'ChelBracket26',
   branch: 'main',
   token:  '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',  // github_pat_...
